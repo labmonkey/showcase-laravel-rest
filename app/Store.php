0bad52a12c6083d6d81fd3162bfe26a257e1cc79
Paweł Derehajło <derehajlo@gmail.com>
2017-03-20 23:26:04 +0000

    allow all properties to be saved

--- app/Store.php
+++ app/Store.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model {
 	protected $table = 'stores';
 
+	protected $fillable = [
+		'storeNumber',
+		'storeName',
+		'address',
+		'siteId',
+		'lat',
+		'lon',
+		'phoneNumber',
+		'cfsFlag'
+	];
+
 	public function getJsonData() {
 		return [
 			'storeNumber' => (int) $this->storeNumber,
