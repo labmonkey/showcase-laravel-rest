f7a2020658a6b5950d8d958ca187dd02aa303415
Paweł Derehajło <derehajlo@gmail.com>
2017-03-21 21:11:47 +0000

    cleanup assets

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,6 +7,7 @@
 require('./bootstrap');
 
 $(document).ready(function () {
+    // The 'Upload' button
     $(".form--server").submit(function (e) {
         e.preventDefault();
         var form = $(this);
@@ -14,10 +15,12 @@ $(document).ready(function () {
         var $list = $forms.find(".list--status");
         var $btn = $forms.find('.btn--download');
 
+        // hide existing results and clean them
         $(".xml-results").addClass('hidden');
         $list.html('');
         $forms.find('.status span').html('');
 
+        // disable button so users won't be able to spam requests
         $btn.addClass("--loading");
         $btn.prop("disabled", true);
 
@@ -26,15 +29,19 @@ $(document).ready(function () {
             url: form.attr('action'),
             data: form.serialize(),
             success: function (data) {
+                // update counters
                 $forms.find('.status--error span').html(data.count.error);
                 $forms.find('.status--success span').html(data.count.success);
                 $forms.find('.status--warning span').html(data.count.updated);
+
+                // add errors to list
                 $.each(data.errors, function (index, value) {
                     var message = "Node (" + index + ") has missing data: " + value.join(', ');
                     $list.append("<li>" + message + "</li>");
                 });
             },
             complete: function () {
+                // enable button and show parsing results
                 $(".xml-results").removeClass('hidden');
                 $btn.removeClass('--loading');
                 $btn.prop("disabled", false);
